7bf25c94440912e3fb88fe2508108ab181aa15aa
Nicole Luong <nickylychie@Nicoles-Air.local>
2022-10-25 15:03:17 +1100

    Updated tutorial code

--- Week07/swap_numbers.c
+++ Week07/swap_numbers.c
@@ -5,17 +5,22 @@
 
 struct person {
     int num;
+    char letter;
 };
 
-void swap_numbers(struct person member1, struct person member2);
+void swap_numbers(struct person *member1, struct person *member2);
 
 int main(void) {
 
     // Create 2 people
     struct person person1;
     person1.num = 1;
+    person1.letter = 'y';
     struct person person2;
     person2.num = 2;
+
+    printf("Address of person1 num: %p\n", &person1.num);
+    printf("Address of person1 letter: %p\n\n", &person1.letter);
 
     // Print numbers
     printf("Person1 Number: %d\n", person1.num);
@@ -23,7 +28,7 @@ int main(void) {
 
     // Swap Numbers
     printf("===== swap =====\n");
-    swap_numbers(person1, person2);
+    swap_numbers(&person1, &person2);
     
     // Print numbers again
     printf("Person1 Number: %d\n", person1.num);
@@ -33,9 +38,9 @@ int main(void) {
 }
 
 // Swaps the numbers of each struct
-void swap_numbers(struct person member1, struct person member2) {
-    int temp_num = member1.num;
+void swap_numbers(struct person *member1, struct person *member2) {
+    int temp_num = member1->num;
 
-    member1.num = member2.num;
-    member2.num = temp_num;
+    member1->num = member2->num;
+    member2->num = temp_num;
 }
